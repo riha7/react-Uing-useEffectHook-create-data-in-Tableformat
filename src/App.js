6b93bd828b9c29255aa1c822658c69089acf7e90
riha7 <harik0725@gmail.com>
2021-09-04 11:47:46 +0530

    using conditional rendering

--- src/App.js
+++ src/App.js
@@ -8,40 +8,43 @@ export default function App() {
   //1st parameter function
   //2nd parameter dependencies
   useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then(response => {
-        let data = response.data;
-        setcount(data);
-      })
+    axios.get('https://jsonplaceholder.typicode.com/todos').then(response => {
+      let data = response.data;
+      setcount(data);
+    });
   }, [increment]);
   console.log(increment);
   return (
     <div>
       <h1>Testing UseEffect Hook calling </h1>
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>name</th>
-              <th>address</th>
-            </tr>
-          </thead>
-          <tbody>
-            {count.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.id}</td>
-                  <td style={{ backgroundColor: 'orange', color: 'white' }}>
-                    {item.name}
-                  </td>
-                  <td style={{ paddingLeft: '50px' }}>{item.address.street}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        {/* conditional rendering */}
+        {increment > 2 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>userId</th>
+                <th>id</th>
+                <th>title</th>
+              </tr>
+            </thead>
+            <tbody>
+              {count.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.userId}</td>
+                    <td style={{ backgroundColor: 'orange', color: 'white' }}>
+                      {item.id}
+                    </td>
+                    <td style={{ paddingLeft: '50px' }}>{item.title}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          <p>data not getting press button again</p>
+        )}
       </div>
       <button
         onClick={() => {
